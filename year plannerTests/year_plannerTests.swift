8c7ce7f428f57e18e654a3617a8820a1cb12cdfc
A.F <akki_mawile23@yahoo.co.jp>
2016-07-03 15:08:55 +0900

    Initial Commit

---
+++ year plannerTests/year_plannerTests.swift
@@ -0,0 +1,36 @@
+//
+//  year_plannerTests.swift
+//  year plannerTests
+//
+//  Created by A.F. on 2016/07/03.
+//  Copyright © 2016年 A.F. All rights reserved.
+//
+
+import XCTest
+@testable import year_planner
+
+class year_plannerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
